289b572a9128da68f64685754f28b286c94ca3d3
Dorban <hallo@dorban.de>
2020-02-11 22:01:47 +0100

    [Favicon] Fixed wrong placed favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,16 +13,16 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       { name: 'msapplication-TileColor', content: '#15c3c5' },
       { name: 'theme-color', content: '#15c3c5' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#15c3c5' }
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#15c3c5' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
     ]
   },
   env: {
