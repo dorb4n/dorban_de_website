c450e4882de781ee4e40fddce7c2ecc83f234687
Dorban <hallo@dorban.de>
2020-02-20 22:49:51 +0100

    [Header] Changed grid

--- components/pageHeader.vue
+++ components/pageHeader.vue
@@ -1,15 +1,15 @@
 <template>
-    <header class="page-header uk-text-bold uk-text-large uk-text-uppercase" data-uk-grid>
-        <div class="page-header_logo uk-width-1-3@m">
-            <nuxt-link to="/">{{ title }}</nuxt-link> <span class="uk-text-normal uk-text-small">Stephan Plöhn</span>
+    <header class="page-header uk-text-bold uk-text-uppercase" data-uk-grid>
+        <div class="page-header_logo uk-width-1-2@s">
+            <nuxt-link to="/">{{ title }}</nuxt-link> <span class="uk-text-normal">Stephan Plöhn</span>
         </div>
-        <nav class="page-header_navigation uk-text-right@m uk-width-expand@m">
-            <ul class="uk-flex-inline uk-list-style-none">
-                <li class="uk-margin-left@m">
-                    <nuxt-link to="/">Start</nuxt-link>
+        <nav class="page-header_navigation uk-width-1-2@s">
+            <ul>
+                <li>
+                    <nuxt-link to="/" aria-label="Zur Startseite">Start</nuxt-link>
                 </li>
-                <li class="uk-margin-left@m">
-                    <nuxt-link to="/posts">Beiträge</nuxt-link>
+                <li>
+                    <nuxt-link to="/posts" aria-label="Beiträge lesen">Beiträge</nuxt-link>
                 </li>
             </ul>
         </nav>
@@ -21,7 +21,20 @@
         &_navigation {
             ul {
                 margin: 0;
+                padding: 0;
                 list-style: none;
+
+                @media (min-width: 640px) {
+                    text-align: right;
+                }
+            }
+            li {
+                display: inline-block;
+                margin-right: 20px;
+                
+                @media (min-width: 640px) {
+                    margin: 0 0 0 20px;
+                }
             }
 
             .nuxt-link-exact-active {
