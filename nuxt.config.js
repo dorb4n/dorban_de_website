e3fea0e2d02eed4721d7dbc65f7f97f14bac47f0
Dorban <hallo@dorban.de>
2020-02-20 22:47:28 +0100

    [Layout] Added some fresh favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,13 +13,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Stephan und das Internet.' },
-      { name: 'msapplication-TileColor', content: '#15c3c5' },
-      { name: 'theme-color', content: '#15c3c5' }
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#15c3c5' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#1c6611' },
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
